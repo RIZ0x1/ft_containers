3a8dc6b39631bd75026773ac614e3216cdddb8d0
RIZ0x1 <riz1.kibiev@gmail.com>
2022-01-15 21:58:47 +0300

    Nothing interestirng

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
-#include <iostream>
-#include <vector>
 #include "vector.hpp"
+#include <vector>
+#include <iostream>
+
+#define NORMAL	"\033[0m"
+#define ORANGE	"\033[31m"
+#define GREEN	"\033[32m"
+#define YELLOW	"\033[33m"
+#define BLUE	"\033[34m"
+#define MAGENTA	"\033[35m"
 
 int main()
 {
 	std::cout << "############### FT_VECTOR ###############\n" << std::endl;
 	std::cout << GREEN;
 	{
-
+		
 	}
 	std::cout << "################  FT_MAP  ###############\n" << std::endl;
-	std::cout << RED;
+	std::cout << ORANGE;
 	{
 
 	}
