1302b2e91020bcfccbf6d804a62e5ece0aa17b58
RIZ0x1 <riz1.kibiev@gmail.com>
2022-01-16 22:05:06 +0300

    Bayinki

--- std_main.cpp
+++ std_main.cpp
@@ -9,10 +9,13 @@
 #define MAGENTA	"\033[35m"
 
 #define TAB '\t'
+#define TAB2 "\t\t"
 
 using std::cout;
 using std::endl;
 using std::vector;
+
+template <typename T> void	test(vector<T> v);
 
 int main()
 {
@@ -20,13 +23,15 @@ int main()
 	cout << "############### STD_VECTOR ###############\n" << std::endl;
 	cout << NORMAL;
 	{
-		vector<int>		v1_int;
-		vector<int>		v2_int();
-		vector<int>		v3_int();
-		vector<int>		v4_int();
+		vector<int>				v1_int;
+		vector<int>				v2_int(0);
+		vector<int>				v3_int();
+		vector<int>				v4_int();
 
-		cout << "CONSTRUCTORS {" << endl;
-		cout << TAB << 
+		test(v1_int);
+		test(v2_int);
+		test(v3_int);
+		test(v4_int);
 		
 	}
 	cout << YELLOW;
@@ -43,4 +48,20 @@ int main()
 	}
 
 	return (0);
+}
+
+template <typename T>
+void	constructors(vector<T> v)
+{
+	vector<int>::iterator	it;
+
+	cout << "CONSTRUCTORS {" << endl;
+		cout << TAB << v1_int.size() << endl;
+		cout << TAB << v1_int.capacity() << endl;
+
+		cout << TAB << "elements {" << endl;
+		it = v1_int.begin();
+		for (it; it != v1_int.end(); it++)
+		{ cout << TAB2 << *it << endl; }
+		cout << TAB << '}' << endl;
 }
