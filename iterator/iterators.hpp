c2e20baebd897c54d47ba99321e9e3e48722983f
RIZ0x1 <riz1.kibiev@gmail.com>
2022-01-26 02:34:26 +0300

    I gues i did it wrong

--- iterator/iterators.hpp
+++ iterator/iterators.hpp
@@ -18,16 +18,16 @@ namespace ft
 		typedef typename iterator<forward_iterator_tag, T>::difference_type	difference_type;
 		typedef typename iterator<forward_iterator_tag, T>::pointer			pointer;
 		typedef typename iterator<forward_iterator_tag, T>::reference		reference;
-		typedef forward_iterator<T>								iterator;
+		typedef forward_iterator<T>											iterator;
 
-	private:
+	protected:
 		pointer	_ptr;
 
 	public:
 		bool					operator == (const forward_iterator<value_type> &other) const;
 		value_type				operator *	(void) const;
 		virtual iterator		operator ++ (void);
-		virtual iterator		operator ++ (int);
+		virtual const iterator		operator ++ (int);
 	};
 
 	// ? ***************************************************************
@@ -38,29 +38,37 @@ namespace ft
 	class bidirectional_iterator : public forward_iterator<T>
 	{
 	public:
-		typedef typename forward_iterator<T>::value_type	value_type;
-		typedef bidirectional_iterator<T>					iterator;
+		typedef typename iterator<forward_iterator_tag, T>::Category		iterator_category;
+		typedef typename iterator<forward_iterator_tag, T>::value_type		value_type;
+		typedef typename iterator<forward_iterator_tag, T>::difference_type	difference_type;
+		typedef typename iterator<forward_iterator_tag, T>::pointer			pointer;
+		typedef typename iterator<forward_iterator_tag, T>::reference		reference;
+		typedef bidirectional_iterator<T>									iterator;
 	public:
 		virtual iterator	    operator -- (void);
 		virtual const iterator	operator -- (int);
 	};
 
     template <typename T>
-    class const_bidirectional_iterator : public bidirectional_iterator<const T>
-    {
-    };
-
-    template <typename T>
 	class reverse_bidirectional_iterator : public bidirectional_iterator<T>
 	{
 	public:
-		typedef typename forward_iterator<T>::value_type	value_type;
-		typedef reverse_bidirectional_iterator<T>			iterator;
+		typedef typename iterator<forward_iterator_tag, T>::Category		iterator_category;
+		typedef typename iterator<forward_iterator_tag, T>::value_type		value_type;
+		typedef typename iterator<forward_iterator_tag, T>::difference_type	difference_type;
+		typedef typename iterator<forward_iterator_tag, T>::pointer			pointer;
+		typedef typename iterator<forward_iterator_tag, T>::reference		reference;
+		typedef reverse_bidirectional_iterator<T>							iterator;
 	public:
-		virtual iterator	operator ++ (void);
-		virtual iterator	operator ++ (int);
-		virtual iterator	operator -- (void);
-		virtual iterator	operator -- (int);
+		iterator		operator ++ (void);
+		const iterator	operator ++ (int);
+		iterator		operator -- (void);
+		const iterator	operator -- (int);
+	};
+
+	template <typename T>
+	class const_bidirectional_iterator : public bidirectional_iterator<const T>
+	{
 	};
 
 	// ? ***************************************************************
@@ -71,27 +79,39 @@ namespace ft
     class random_access_iterator : public bidirectional_iterator<T>
 	{
     public:
-		typedef typename forward_iterator<T>::difference_type	difference_type;
-		typedef random_access_iterator<T>						iterator;
+		typedef typename iterator<forward_iterator_tag, T>::Category		iterator_category;
+		typedef typename iterator<forward_iterator_tag, T>::value_type		value_type;
+		typedef typename iterator<forward_iterator_tag, T>::difference_type	difference_type;
+		typedef typename iterator<forward_iterator_tag, T>::pointer			pointer;
+		typedef typename iterator<forward_iterator_tag, T>::reference		reference;
+		typedef random_access_iterator<T>									iterator;
 	public:
-		bool				operator < (const random_access_iterator &other);
-		bool				operator > (const random_access_iterator &other);
-		bool				operator <= (const random_access_iterator &other);
-		bool				operator >= (const random_access_iterator &other);
-		virtual iterator	operator += (const difference_type N);
-		virtual iterator	operator -= (const difference_type N);
+		bool				operator < (const iterator &other);
+		bool				operator > (const iterator &other);
+		bool				operator <= (const iterator &other);
+		bool				operator >= (const iterator &other);
 		virtual iterator	operator + (const difference_type N);
 		virtual iterator	operator - (const difference_type N);
+		virtual iterator	operator += (const difference_type N);
+		virtual iterator	operator -= (const difference_type N);
 	};
 
 	template <typename T>
-	class reverse_random_access_iterator : public reverse_bidirectional_iterator<T>
+	class reverse_random_access_iterator : public random_access_iterator<T>, public reverse_bidirectional_iterator<T>
 	{
 	public:
-		typedef typename forward_iterator<T>::difference_type	difference_type;
-		typedef reverse_random_access_iterator					iterator;
+		typedef typename iterator<forward_iterator_tag, T>::Category		iterator_category;
+		typedef typename iterator<forward_iterator_tag, T>::value_type		value_type;
+		typedef typename iterator<forward_iterator_tag, T>::difference_type	difference_type;
+		typedef typename iterator<forward_iterator_tag, T>::pointer			pointer;
+		typedef typename iterator<forward_iterator_tag, T>::reference		reference;
+		typedef reverse_random_access_iterator								iterator;
 	public:
 
+		iterator	operator + (const difference_type N);
+		iterator	operator - (const difference_type N);
+		iterator	operator += (const difference_type N);
+		iterator	operator -= (const difference_type N);
 	};
 
     template <typename T>
--- iterator/iterators.tpp
+++ iterator/iterators.tpp
@@ -7,15 +7,15 @@
 using ft::forward_iterator;
 
 template <typename value_type>
-typename forward_iterator<value_type>::value_type	forward_iterator<value_type>::operator * () const
-{
-    return (*_ptr);
-}
-
-template <typename value_type>
 bool	forward_iterator<value_type>::operator == (const forward_iterator<value_type> &other) const
 {
 	return (this->ptr == other.ptr);
+}
+
+template <typename value_type>
+typename forward_iterator<value_type>::value_type	forward_iterator<value_type>::operator * () const
+{
+    return (*_ptr);
 }
 
 template <typename value_type>
@@ -29,7 +29,7 @@ typename forward_iterator<value_type>::iterator	forward_iterator<value_type>::op
 }
 
 template <typename value_type>
-typename forward_iterator<value_type>::iterator	forward_iterator<value_type>::operator ++ (int)
+const typename forward_iterator<value_type>::iterator	forward_iterator<value_type>::operator ++ (int)
 {
 	(_ptr++);
 	return (*_ptr);
@@ -40,25 +40,67 @@ typename forward_iterator<value_type>::iterator	forward_iterator<value_type>::op
 // ? ***************************************************************************
 
 using ft::bidirectional_iterator;
+using ft::reverse_bidirectional_iterator;
 
 template <typename value_type>
 typename bidirectional_iterator<value_type>::iterator    bidirectional_iterator<value_type>::operator -- ()
 {
-
+	(this->_ptr--);
+	return (*(this->_ptr));
 }
 
 template <typename value_type>
-typename bidirectional_iterator<value_type>::iterator	bidirectional_iterator<value_type>::operator -- (int)
+const typename bidirectional_iterator<value_type>::iterator	bidirectional_iterator<value_type>::operator -- (int)
 {
+	pointer	tmp;
 
+	tmp = this->_ptr;
+	(this->_ptr--);
+	return (*tmp);
 }
 
-virtual iterator	operator -- ()
-{
+// * ********************* REVERSE BIDIRECTIONAL ITERATOR **********************
 
+template <typename value_type>
+typename reverse_bidirectional_iterator<value_type>::iterator    reverse_bidirectional_iterator<value_type>::operator ++ ()
+{
+	(this->_ptr--);
+	return (*(this->_ptr));
 }
 
-virtual iterator	operator -- (int)
+template <typename value_type>
+const typename reverse_bidirectional_iterator<value_type>::iterator	reverse_bidirectional_iterator<value_type>::operator ++ (int)
 {
+	pointer	tmp;
 
+	tmp = this->_ptr;
+	(this->_ptr--);
+	return (*tmp);
 }
+
+template <typename value_type>
+typename reverse_bidirectional_iterator<value_type>::iterator    reverse_bidirectional_iterator<value_type>::operator -- ()
+{
+	pointer	tmp;
+
+	this->_ptr;
+
+	tmp = this->_ptr;
+	(this->_ptr++);
+	return (*tmp);
+}
+
+template <typename value_type>
+const typename reverse_bidirectional_iterator<value_type>::iterator	reverse_bidirectional_iterator<value_type>::operator -- (int)
+{
+	this->_ptr++;
+	return *(this->_ptr);
+}
+
+// ? ***************************************************************************
+// ? *                          RANDOM ACESS ITERATOR                          *
+// ? ***************************************************************************
+
+
+
+
