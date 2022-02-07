bec8b0c4821c5461dff9dc6d27ef12943f6d5187
RIZ0x1 <riz1.kibiev@gmail.com>
2022-02-07 16:39:43 +0300

    Decommented. Extra lines removed

--- tests/ft_main.cpp
+++ tests/ft_main.cpp
@@ -17,67 +17,18 @@
 using std::cout;
 using std::endl;
 
-template <typename T> void	test(ft::vector<T> v);
+#include <vector>
+
+template <typename T> void	print_info(vector<T> v);
 
 int main()
 {
-	cout << YELLOW;
-	cout << "############### FT_VECTOR ###############\n" << std::endl;
-	cout << NORMAL;
-	{
-		// ? CONSTRUCTORS
-		// vector<int>				v1_int;
-		ft::vector<int>				v2_int(500);
-		ft::vector<int>				v3_int(0);
+	ft::vector<int>		v(10);
 
-		//test(v1_int);
-		test(v2_int);
-		test(v3_int);
+	v.push_back(69);
+	v.push_back(70);
 
-		try {
-			ft::vector<int>	v4_int(-1);
-		} catch (std::exception &e) {
-			 cout << ORANGE << "EXCEPTION: " << e.what() << endl << endl; 
-		}
-
-		// ? MEMBER FUNCTIONS
-		// const std::allocator<int>	alloc;
-
-		// vector<int> tmp(alloc);
-
-		// tmp.push_back(100);
-
-		// test(tmp);
-
-		// ? MODIFIERS & CAPACITY
-		// v1_int.push_back(INT_MIN);
-		// v1_int.push_back(INT_MAX);
-		// test(v1_int);
-
-		// for (unsigned i = 0; i < v2_int.size(); i++)
-		// 	v2_int[i] = i;
-		// test(v2_int);
-
-		// if (v3_int.empty())
-		// {
-		// 	for (unsigned i = 0; i < 72; i++)
-		// 		v3_int.push_back(i);
-		// 	test(v3_int);
-		// }
-		
-	}
-	cout << YELLOW;
-	cout << "################  FT_MAP  ###############\n" << std::endl;
-	cout << NORMAL;
-	{
-
-	}
-	cout << YELLOW;
-	cout << "################ FT_STACK ###############\n" << std::endl;
-	cout << NORMAL;
-	{
-
-	}
+	print_info(v);
 
 	return (0);
 }
@@ -85,9 +36,9 @@ int main()
 #define COLUMNS 10
 
 template <typename T>
-void	test(ft::vector<T> v)
+void	print_info(ft::vector<T> v)
 {
-	static unsigned int	number = 1;
+	static short number = 1;
 
 	cout << CYAN << "TEST #" << number++ << " {" << endl;
 		cout << GREEN	<< TAB << "size: " << v.size() << endl;
@@ -95,18 +46,18 @@ void	test(ft::vector<T> v)
 		cout << NORMAL	<< TAB << "capacity: " << v.capacity() << endl;
 
 		unsigned int			i;
-		// vector<int>::iterator	it;
+		ft::vector<int>::iterator	it;
 
-		// cout << TAB << MAGENTA << "elements: {" << endl;
-		// it = v.begin();
-		// while (it != v.end())
-		// {
-		// 	i = 0;
-		// 	cout << TAB2;
-		// 	while (it != v.end() && i++ < COLUMNS)
-		// 		cout << *it++ << ' ';
-		// 	cout << endl;
-		// }
-		// cout << TAB2 << '}' << endl;
+		cout << TAB << MAGENTA << "elements: {" << endl;
+		it = v.begin();
+		while (it != v.end())
+		{
+			i = 0;
+			cout << TAB2;
+			while (it != v.end() && i++ < COLUMNS)
+				cout << *it++ << ' ';
+			cout << endl;
+		}
+		cout << TAB2 << '}' << endl;
 	cout << CYAN << TAB << '}' << endl << endl;
 }
