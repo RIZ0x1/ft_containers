f0ed8f465056d0513b19a4d6fd6af42c88969eea
Jeremiah Carlena <jcarlena@ge-h6.kzn.21-school.ru>
2022-03-08 18:28:11 +0300

    Lexicographical compare added

---
+++ ft_utils/lexicographical_compare.hpp
@@ -0,0 +1,15 @@
+#ifndef LEXICOGRAPHICAL_COMPARE_HPP
+# define LEXICOGRAPHICAL_COMPARE_HPP
+
+template <typename Iterator1, typename Iterator2>
+bool	lexicographical_compare(Iterator1 start1, Iterator1 end1, Iterator2 start2, Iterator2 end2)
+{
+	for (; start1 != end1 && start2 != end2; start1++, start2++)
+	{
+		if (*start1 < *start2) return (true);
+		if (*start1 > *start2) return (false);
+	}
+	return ( (start1 == end1) && (start2 != end2) );
+}
+
+#endif // LEXICOGRAPHICAL_COMPARE_HPP
