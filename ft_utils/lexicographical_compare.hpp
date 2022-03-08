308d725476e1b2ca351b45c0619fc9d9d1186f63
Jeremiah Carlena <jcarlena@ge-h6.kzn.21-school.ru>
2022-03-08 18:37:26 +0300

    IDE, dont ever touch my code!

--- ft_utils/lexicographical_compare.hpp
+++ ft_utils/lexicographical_compare.hpp
@@ -4,8 +4,10 @@
 namespace ft
 {
 	template<typename Iterator1, typename Iterator2>
-	bool lexicographical_compare(Iterator1 start1, Iterator1 end1, Iterator2 start2, Iterator2 end2) {
-		for (; start1 != end1 && start2 != end2; start1++, start2++) {
+	bool lexicographical_compare(Iterator1 start1, Iterator1 end1, Iterator2 start2, Iterator2 end2)
+	{
+		for (; start1 != end1 && start2 != end2; start1++, start2++)
+		{
 			if (*start1 < *start2) return (true);
 			if (*start1 > *start2) return (false);
 		}
